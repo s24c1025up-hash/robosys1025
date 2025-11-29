d485edaa781e5b304dd37e13c1ab26a844d632f3
Ryuse Oshiba <S24C1025UP@s.chibakoudai.jp>
2025-11-29 22:07:46 +0900

    plusを削除

--- test.bash
+++ test.bash
@@ -1,12 +1,14 @@
-#!/bin/bash
-ng () {
-        echo ${1}行目が違うよ
-        res=1                   #追加
-}
+#!/bin/bash -xv
+           ### 省略しますが、ここに前回学んだ著作権やライセンスの設定を！！
+ ng () {
+         echo ${1}行目が違うよ
+         res=1
+ }
 
-res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数         
-[ "$a" = 山田 ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
+ res=0    
 
-exit $res     # このシェルスクリプトの終了ステータスを返して終了
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+ 
+[ "${res}" = 0 ] && echo OK #通ったのが（人間に）分かるように表示
+ exit $res
